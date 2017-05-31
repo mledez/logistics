b85e58b23ae94e2c68c6254b5365f7b982cdc048
Miguel Ledezma <ledezma.miguel@gmail.com>
2017-05-30 19:01:05 -0500

    Project Completed

--- logistics/src/logistics/Main.java
+++ logistics/src/logistics/Main.java
@@ -1,5 +1,7 @@
 package logistics;
 
+import java.util.ArrayList;
+
 import logistics.exceptions.ClassInstantiationException;
 import logistics.exceptions.DuplicatedDataException;
 import logistics.exceptions.InitializationException;
@@ -12,28 +14,72 @@ import logistics.order.OrderManager;
 
 public class Main {
 	public static void main(String[] args) {
-		NetworkManager nm = NetworkManager.getInstance();
-		ItemManager im = ItemManager.getInstance();
-		FacilityManager fm = FacilityManager.getInstance();
-		OrderManager om = OrderManager.getInstance();
 
 		try {
-			nm.init("Links.xml", 8, 50);
-			im.init("Items.xml");
-			fm.init("Facilities.xml");
-			om.init("Orders.xml", 500);
-			om.processOrders();
-
-			System.out.println(new String(new char[82]).replace("\0", "-"));
-
-			System.out.print(fm.getReport());
-			System.out.print(om.getProcessingReport());
-			System.out.print(fm.getReport());
-
+			phaseTwo();
 		} catch (XmlReadingException | InvalidDataException | InitializationException | DuplicatedDataException
 				| ClassInstantiationException e) {
 			System.err.println(e.getMessage());
 		}
 	}
 
+	public static void phaseTwo() throws InitializationException, InvalidDataException, XmlReadingException,
+			DuplicatedDataException, ClassInstantiationException {
+
+		NetworkManager nm = NetworkManager.getInstance();
+		ItemManager im = ItemManager.getInstance();
+		FacilityManager fm = FacilityManager.getInstance();
+		OrderManager om = OrderManager.getInstance();
+
+		nm.init("Links.xml", 8, 50);
+		im.init("Items.xml");
+		fm.init("Facilities.xml");
+		om.init("Orders.xml", 500);
+
+		System.out.println(new String(new char[82]).replace("\0", "-"));
+
+		System.out.print(fm.getReport());
+
+		om.processOrders();
+		System.out.print(om.getProcessingReport());
+
+		System.out.print(fm.getReport());
+
+	}
+
+	public static void phaseOne() throws DuplicatedDataException, ClassInstantiationException, XmlReadingException,
+			InvalidDataException, InitializationException {
+
+		NetworkManager nm = NetworkManager.getInstance();
+		ItemManager im = ItemManager.getInstance();
+		FacilityManager fm = FacilityManager.getInstance();
+
+		ArrayList<String[]> samplePairs = new ArrayList<>();
+		samplePairs.add(new String[] { "Santa Fe, NM", "Chicago, IL" });
+		samplePairs.add(new String[] { "Atlanta, GA", "St. Louis, MO" });
+		samplePairs.add(new String[] { "Seattle, WA", "Nashville, TN" });
+		samplePairs.add(new String[] { "New York City, NY", "Phoenix, AZ" });
+		samplePairs.add(new String[] { "Fargo, ND", "Austin, TX" });
+		samplePairs.add(new String[] { "Denver, CO", "Miami, FL" });
+		samplePairs.add(new String[] { "Austin, TX", "Norfolk, VA" });
+		samplePairs.add(new String[] { "Miami, FL", "Seattle, WA" });
+		samplePairs.add(new String[] { "Los Angeles, CA", "Chicago, IL" });
+		samplePairs.add(new String[] { "Detroit, MI", "Nashville, TN" });
+
+		nm.init("Links.xml", 8, 50);
+		im.init("Items.xml");
+		fm.init("Facilities.xml");
+
+		System.out.println(fm.getReport());
+		System.out.println(im.getReport());
+
+		char letter = 'a';
+		String report = "Shortest Path Tests:\n\n";
+		for (String[] pair : samplePairs) {
+			report += nm.getPathReport(pair[0], pair[1]).replaceAll("(?m)(^.*\\n)(^.*\\n)+(^.*$)",
+					letter + ") $1   •  $2   •  $3\n\n");
+			letter++;
+		}
+		System.out.println(report);
+	}
 }
